bd71d3f80564b76a6b85f65d29fd1d7297660c2b
armannella <armannella2@gmail.com>
2026-06-18 05:41:29 +0200

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\Decision;
 use App\Models\Factor;
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
         Gate::define('isFactorForDecision' , function(User $user, Decision $decision , Factor $factor){
             if($decision->id == $factor->decision_id){
                 return true;
